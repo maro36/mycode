e996d63fb15f96b117fcc957cec30bcf5d9fd721
maro36 <maro36@users.noreply.github.com>
2022-05-24 20:59:00 +0000

    manipulating lists

---
+++ listmethods/listmeth01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Define list items
+proto = ["ssh", "http", "https"]
+
+#print full list
+print(proto)
+
+#print second list item
+print(proto[1])
+
+proto.extend("dns")
+print( proto)
