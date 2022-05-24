624dbb6ba0c9802b80b4d9cc7404c2cc3dda62ef
maro36 <maro36@users.noreply.github.com>
2022-05-24 19:59:24 +0000

    mixed lists

---
+++ mix-list/mixlist_challenge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#Define list items
+my_list = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#Print out list items
+print("Your IP addresses are: " + my_list[3] + " and " + my_list[4])
+
